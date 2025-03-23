23544713ec66c582d7c487e4f22d69715a3209e6
St-Costa <stefanocosta480@gmail.com>
2025-03-23 15:02:04 +0100

    [FIX] Improved footer with messages and links

--- javascript/footer.js
+++ javascript/footer.js
@@ -1,7 +1,8 @@
 document.write(`
     <footer>
-        <div class="footer-link center">
+        <div>
             <a href="../index.html">Home</a>
+            <p>Handmade site. <a target="_blank" href="https://www.reddit.com/r/ProgrammerHumor/comments/15gde8z/thehiddentruth/#lightbox">No React</a>. Raw HTML, vanilla CSS, a few drops of JS I couldn't avoid</p>
         </div>
     </footer>
         `);
