30db66b662b58e9f2dd8144c955939593b3ce468
St-Costa <stefanocosta480@gmail.com>
2025-04-04 21:10:31 +0200

    [ADD] New blog post

---
+++ javascript/toc.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const tocContainer = document.querySelector(".toc");
+    const headers = document.querySelectorAll("h2, h3");
+
+    let tocHTML = "";
+
+    headers.forEach((header, index) => {
+        if (index < 2) return;
+        header_string = header.textContent.trim();
+        
+        header_string = header_string.replace("■", "");
+        header_string = header_string.replace("└─", "");
+        
+        const nextElement = headers[index + 1];
+        const level = header.tagName === "H2" 
+            ? "■" 
+            : (nextElement && nextElement.tagName !== "H2" ? "├─" : "└─");
+        const link = `<a href="#${header.id}">${header_string}</a>`;
+        tocHTML += `${level} ${link}<br>`;
+        if (nextElement && nextElement.tagName === "H2") {
+            tocHTML += "<div style='height:3ch; display:inline-block;'></div>";
+        }
+    });
+
+    tocContainer.innerHTML += tocHTML;
+});
