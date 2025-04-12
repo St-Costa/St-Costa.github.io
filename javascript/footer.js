9f05fac131fc034faa49f74d483e5cdfab7805fa
St-Costa <stefanocosta480@gmail.com>
2025-04-12 11:58:56 +0200

    [ADD] Colophon and update the footer

--- javascript/footer.js
+++ javascript/footer.js
@@ -1,8 +1,6 @@
 document.write(`
     <footer>
-        <div>
-            <a href="../index.html">Home</a>
-            <p><a target="_blank" href="https://www.reddit.com/r/ProgrammerHumor/comments/15gde8z/thehiddentruth/#lightbox">Handmade site</a>. Raw HTML + CSS, few drops of JS. <a target="_blank" href="https://www.jetbrains.com/lp/mono/">JetBrains Mono</a>.</p>
-        </div>
+            <a href="../index.html">Home</a>&nbsp;&nbsp;
+            <a href="./colophon.html">Colophon</a>
     </footer>
         `);
--- javascript/footer_blogPost.js
+++ javascript/footer_blogPost.js
@@ -1,12 +1,7 @@
 document.write(`
     <footer>
-        <div>
-            
-                <a href="../index.html">Home</a>
-                <div style="width: 20px; display: inline-block;"></div>
-                <a href="../mainPages/Blog_pages.html">Blog</a>
-            
-            <p><a target="_blank" href="https://www.reddit.com/r/ProgrammerHumor/comments/15gde8z/thehiddentruth/#lightbox">Handmade site</a>. Raw HTML + CSS, few drops of JS. <a target="_blank" href="https://www.jetbrains.com/lp/mono/">JetBrains Mono</a>.</p>
-        </div>
+        <a href="../index.html">Home</a>&nbsp;&nbsp;
+        <a href="../mainPages/Blog_pages.html">Blog</a>&nbsp;&nbsp;
+        <a href="../mainPages/colophon.html">Colophon</a>
     </footer>
         `);
